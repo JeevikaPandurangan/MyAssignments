456329f36f914e271f30ffbf9efaef8879607a32
Jeevika P <64584094+JeevikaPandurangan@users.noreply.github.com>
2023-04-22 20:41:33 +0530

    Add files via upload
    Week 1

---
+++ week1/day1/Browser.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		Chrome chrome = new Chrome();
+		chrome.getName();
+		chrome.PrintName();
+		System.out.println("This is my Browser");
+		
+
+	}
+
+}
---
+++ week1/day1/Report.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		System.out.println(s1.studentName);
+		System.out.println(s1.rollno);
+		System.out.println(s1.collegeName);
+		System.out.println(s1.markScored);
+		System.out.println(s1.cgpa);
+
+	}
+
+}
